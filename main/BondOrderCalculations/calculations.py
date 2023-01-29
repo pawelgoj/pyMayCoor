9c6e308e52a90f46da06552c71addd5857304688
Pawel <71873238+pawelgoj@users.noreply.github.com>
2023-01-29 19:56:01 +0100

    Add Histogram functionality

--- main/BondOrderCalculations/calculations.py
+++ main/BondOrderCalculations/calculations.py
@@ -1,16 +1,55 @@
-from input_data import MayerBondOrders
+import numpy as np
+from typing import Callable
 
 
 class Histogram:
-    numbers_of_bars: int = 0
-    mayer_bond_orders: MayerBondOrders | None = None
+    """Methods to generate histograms"""
+    histogram: Callable = np.histogram
+    x: list[float] = []
+    y: list[int] = []
 
-    def __init__(self, numbers_of_bars: int = 0,
-                 mayer_bond_orders: MayerBondOrders = None) -> None:
-        self.mayer_bond_orders = mayer_bond_orders
-        self.numbers_of_bars = numbers_of_bars
+    @classmethod
+    def calculate_histogram(cls, values: list[float], bins: int)\
+            -> tuple[list[float], list[int]]:
+        """Calculate histogram.
 
-    def calculate_histogram(atom_symbol_1: str, atom_symbol_2: str)\
-            -> tuple(list(float), list(int)):
-        # TODO
-        pass
+        Args:
+            values (list[float]): list of values
+            bins (int): number of bins in histogram
+
+        Returns:
+            tuple[list[float], list[int]]: (list of x, list of y)
+        """
+        histogram = cls.histogram(values, bins)
+
+        y = histogram[0]
+        x = histogram[1]
+        y = y.tolist()
+        x = x.tolist()
+
+        first_loop = True
+        new_x = []
+        for item in x:
+            if first_loop:
+                first_loop = False
+                previous = item
+            else:
+                new_x.append((item
+                              + previous) / 2)
+                previous = item
+
+        histogram = Histogram()
+        histogram.x = new_x
+        histogram.y = y
+        return histogram
+
+    def to_string(self, atom_symbol_1: str, atom_symbol_2: str)\
+            -> str:
+        string = atom_symbol_1 + ', ' + atom_symbol_2 + '\n\n'
+        string = string + 'Interval/2' + ' ' + 'Count' + '\n\n'
+        for i in range(len(self.x)):
+            string = string + str(self.x[i]) + ' ' + str(self.y[i]) + '\n'
+
+        string = string + '\n'
+
+        return string
---
+++ test/test_calculations.py
@@ -0,0 +1,26 @@
+import pytest
+from main.BondOrderCalculations.calculations import Histogram
+
+from pprint import pprint
+
+
+class TestHistogram:
+    values = [2, 2.5, 3, 5, 6, 7, 6, 3, 3.75, 2.5]
+
+    def test_calculate_histogram(self):
+
+        result = Histogram.calculate_histogram(self.values, 3)
+        assert (result.x == [2.8333333333333335, 4.5, 6.166666666666667]
+                and result. y == [5, 2, 3])
+
+    def test_to_string(self):
+
+        string = Histogram.calculate_histogram(self.values, 3)\
+            .to_string('P', 'O')
+
+        assert string == ("P, O\n"
+                          + "\n"
+                          + "Interval/2 Count\n\n"
+                          + "2.8333333333333335 5\n"
+                          + "4.5 2\n"
+                          + "6.166666666666667 3\n\n")
