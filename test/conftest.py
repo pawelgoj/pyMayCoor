bb11f2ee348d3912a975e7809ccc7d7ae47d69f4
Pawel <71873238+pawelgoj@users.noreply.github.com>
2023-02-02 16:47:18 +0100

    Refactor

--- test/conftest.py
+++ test/conftest.py
@@ -5,7 +5,7 @@ from main.BondOrderCalculations.input_data import LoadedData
 
 @pytest.fixture()
 def path_to_input_file(request):
-    path_to_input_file = "egzamples_instructions\out1.txt"
+    path_to_input_file = "egzamples_instructions/out1.txt"
 
     yield path_to_input_file
 
@@ -13,7 +13,7 @@ def path_to_input_file(request):
 @pytest.fixture()
 def input_data_object_with_loaded_data(request):
     input_data = InputDataFromCPMD()
-    path_to_input_file = "egzamples_instructions\out1.txt"
+    path_to_input_file = "egzamples_instructions/out1.txt"
     input_data.load_input_data(path_to_input_file,
                                LoadedData.UnitCell,
                                LoadedData.MayerBondOrders,
