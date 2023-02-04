e38a6a4d7fb92143519a0ee3834f2fe2a5336da6
Pawel <71873238+pawelgoj@users.noreply.github.com>
2023-02-04 17:30:10 +0100

    Action

--- run_tests.bat
+++ run_tests.bat
@@ -3,4 +3,4 @@ pytest --doctest-modules main/BondOrderCalculations/BondOrderCalculations/input_
 pytest --doctest-modules main/BondOrderCalculations/BondOrderCalculations/__init__.py
 pytest --doctest-modules main/Settings/settings.py
 :: Normal unit tests
-pytest -s --cov=main\BondOrderCalculations\BondOrderCalculations test/ > pytest-converage.txt
+pytest -s --cov=main/BondOrderCalculations/BondOrderCalculations test/ > pytest-converage.txt
