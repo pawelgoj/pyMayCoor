f8bea931112f220bead9793f4c1ac5536e520ee4
Pawel <71873238+pawelgoj@users.noreply.github.com>
2023-02-11 16:44:51 +0100

    Update tests

--- test/test_end_to_end.py
+++ test/test_end_to_end.py
@@ -8,7 +8,7 @@ class TestEndToEnd:
         fn = env_for_end_to_end_tests_2
 
         subprocess.run(
-            f'python3_10 ./main/main.py -i ./egzamples_instructions/out1.txt -s ./egzamples_instructions/settings.yaml -o "{fn}"', shell=True)
+            f'python ./main/main.py -i ./egzamples_instructions/out1.txt -s ./egzamples_instructions/settings.yaml -o "{fn}"', shell=True)
 
         with open(fn, 'r', encoding="utf-8") as file:
             data = file.read()
